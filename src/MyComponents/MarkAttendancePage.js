f3815a4fdb6b2adc77ce8bc6482917c190230c4a
Rahul Madan <madan.rahul9@gmail.com>
2022-09-23 03:21:24 +0530

    added notification on mark attendance page

--- src/MyComponents/MarkAttendancePage.js
+++ src/MyComponents/MarkAttendancePage.js
@@ -2,12 +2,25 @@ import React from "react";
 import {useNavigate} from "react-router-dom";
 import {useState, useEffect} from "react";
 
+
 import axios from "axios";
+import 'react-toastify/dist/ReactToastify.css'
+import {toast, ToastContainer} from "react-toastify";
 
 export const MarkAttendancePage = (props) => {
 
+    const notify = () => toast('Wow so easy!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
     const [userLatitude, setUserLatitude] = useState(0)
     const [userLongitude, setUserLongitude] = useState(0)
+    const [userIPv4, setUserIPv4] = useState(0)
 
 
 
@@ -16,16 +29,34 @@ export const MarkAttendancePage = (props) => {
         navigate(path);
     }
 
-    const markAttendanceButtonClick = (e) => {
-        e.preventDefault()
-        navigator.geolocation.getCurrentPosition(function(position) {
+    const getIPv4 = async () => {
+        const res = await axios.get('https://geolocation-db.com/json/')
+        console.log(res.data);
+        setUserIPv4(res.data.IPv4)
+        notify()
+    }
+
+
+    const getLocation = async () => {
+        await navigator.geolocation.getCurrentPosition(function(position) {
             console.log("Latitude is :", position.coords.latitude);
             console.log("Longitude is :", position.coords.longitude);
             setUserLatitude(position.coords.latitude)
             setUserLongitude(position.coords.longitude)
         },function (error){
-            console.log(error.message)
-        },{enableHighAccuracy: true, timeout: 50000});
+            console.log(error)
+            console.log("Location access do")
+        },{enableHighAccuracy: true, timeout: 10000, showLocationDialog: true,
+            forceRequestLocation: true});
+    }
+
+
+
+    const markAttendanceButtonClick = async (e) => {
+        e.preventDefault()
+        getLocation()
+        getIPv4()
+
     }
 
 
@@ -39,6 +70,20 @@ export const MarkAttendancePage = (props) => {
             </div>
             <p>Latitude: {userLatitude}</p>
             <p>Longitude: {userLongitude}</p>
+            <p>IPv4 Address: {userIPv4}</p>
+
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+
         </>
     )
 }
